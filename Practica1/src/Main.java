d5836588b39c9be3da3d8d07b0c7858738ec63b6
JoelTorres981 <joel.torres@epn.edu.ec>
2024-10-04 13:02:22 -0500

    Add files via upload

---
+++ Practica1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Joel Torres");
+        System.out.println("20");
+        System.out.println("0963663076");
+        System.out.println("quitumbe");
+
+    }
+}
